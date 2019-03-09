0e6d4edb8af2b091ccea585ff3b280864577d1b5
Zahndrekh <mark.tomlinson@outlook.fr>
2019-03-09 10:41:04 +0100

    Create PlayerCtrl + terrain

---
+++ Assets/Script/PlayerCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerCtrl : MonoBehaviour {
+	
+	Vector3 move;
+	Rigidbody2D rb;
+	public float speed = 4;
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Move();
+	}
+
+		void Move()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+	
+		move = new Vector3 (x, y, 0);
+		rb.velocity = (move * speed * Time.deltaTime);
+		//transform.position += move * speed * Time.deltaTime;
+	}
+}
